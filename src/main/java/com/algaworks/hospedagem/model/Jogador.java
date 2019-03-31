1186a9d3685c4ee908c705854a2b22dc8bbfeb89
Evandro Silva <evandro.ms2016@gmail.com>
2019-03-31 02:00:12 -0400

    primeiro commit

--- src/main/java/com/algaworks/hospedagem/model/Jogador.java
+++ src/main/java/com/algaworks/hospedagem/model/Jogador.java
@@ -8,7 +8,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.TableGenerator;
 
-//@Entity
+@Entity
 public class Jogador implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
