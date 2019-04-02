1344a47939da4616816bd709204f64e5abf271d1
Evandro Silva <evandro.ms2016@gmail.com>
2019-04-02 17:03:41 -0400

    primeiro commit

---
+++ src/main/java/com/webapp/controller/SimularOperacaoBean.java
@@ -0,0 +1,22 @@
+package com.webapp.controller;
+
+import java.io.Serializable;
+
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class SimularOperacaoBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	
+	public void inicializar() {
+
+		
+
+	}
+
+	
+}
